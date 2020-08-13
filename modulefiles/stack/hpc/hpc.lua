f7a04b73d076da02718df2dfd7a150568e60f327
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-08-13 21:05:48 +0000

    LMOD defaults on wcoss are draconian

--- modulefiles/stack/hpc/hpc.lua
+++ modulefiles/stack/hpc/hpc.lua
@@ -12,6 +12,10 @@ setenv("HPC_OPT", opt)
 local mpath = pathJoin(opt,"modulefiles/core")
 prepend_path("MODULEPATH", mpath)
 
+-- https://lmod.readthedocs.io/en/latest/090_configuring_lmod.html
+setenv("LMOD_EXACT_MATCH", "no")
+setenv("LMOD_EXTENDED_DEFAULT", "yes")
+
 whatis("Name: ".. pkgName)
 whatis("Version: " .. pkgVersion)
 whatis("Category: Base")
