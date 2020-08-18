33aba185bde1fbc795a34b4e53794407b6349ca4
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-08-18 15:38:03 -0400

    Feature/hera (#6)
    * add hera configs
    
    * update compiler and mpi combo
    
    * remove tar.gz after download
    
    * hera config

--- libs/build_udunits.sh
+++ libs/build_udunits.sh
@@ -38,7 +38,7 @@ cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}
 
 software=$name-$version
 url=ftp://ftp.unidata.ucar.edu/pub/udunits/$software.tar.gz
-[[ -d $software ]] || ( $WGET $url; tar xvf $software.tar.gz )
+[[ -d $software ]] || ( $WGET $url; tar xvf $software.tar.gz && rm -f $software.tar.gz )
 [[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
 [[ -d $software ]] && cd $software || ( echo "$software does not exist, ABORT!"; exit 1 )
 [[ -d build ]] && rm -rf build
