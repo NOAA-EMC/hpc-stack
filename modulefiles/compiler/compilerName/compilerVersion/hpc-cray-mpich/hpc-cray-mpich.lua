6673e180acbfa2aef536f784e786ac6078d7641d
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-08-25 22:18:17 +0000

    initial commit for wcoss2.  Some peculiarities wrt cray-mpich and intel etc.  this is first time encountering Cray

---
+++ modulefiles/compiler/compilerName/compilerVersion/hpc-cray-mpich/hpc-cray-mpich.lua
@@ -0,0 +1,30 @@
+help([[
+]])
+
+local pkgName    = myModuleName()
+local pkgVersion = myModuleVersion()
+local pkgNameVer = myModuleFullName()
+
+local hierA        = hierarchyA(pkgNameVer,1)
+local compNameVer  = hierA[1]
+local compNameVerD = compNameVer:gsub("/","-")
+
+conflict(pkgName)
+conflict("hpc-openmpi","hpc-impi")
+
+local mpi = pathJoin("cray-mpich",pkgVersion)
+load(mpi)
+prereq(mpi)
+
+local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
+local mpath = pathJoin(opt,"modulefiles/mpi",compNameVer,"cray-mpich",pkgVersion)
+prepend_path("MODULEPATH", mpath)
+
+setenv("MPI_FC",  "ftn")
+setenv("MPI_CC",  "cc")
+setenv("MPI_CXX", "CC")
+
+whatis("Name: ".. pkgName)
+whatis("Version: " .. pkgVersion)
+whatis("Category: library")
+whatis("Description: MPICH Library and module access")
