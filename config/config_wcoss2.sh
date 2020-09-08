aa6f73a2471b16510e4f57912f3121f623a840e9
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-09-08 17:37:59 -0400

    merge develop
    Change-Id: Ibbb444bdeeacd1ac68737085912a3209c28ef104

--- config/config_wcoss2.sh
+++ config/config_wcoss2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Compiler/MPI combination
-export HPC_COMPILER="cray-intel/19.1.1.217"  # See IMPORTANT NOTE below
+#export HPC_COMPILER="cray-intel/19.1.1.217"  # See IMPORTANT NOTE below
+export HPC_COMPILER="intel/19.1.1.217"
 export HPC_MPI="cray-mpich/8.0.11"
 
 # Build options
@@ -17,6 +18,7 @@ export DOWNLOAD_ONLY=F
 export STACK_EXIT_ON_FAIL=Y
 export WGET="wget -nv"
 
+# WCOSS2 specific
 # NOTE:
 # On WCOSS2 the Intel compiler module is "intel/19.1.1.217"
 # However, on the Cray, the wrappers are cc, ftn, and CC
@@ -27,7 +29,6 @@ export WGET="wget -nv"
 # cray-intel does not imply the native module in this case
 
 module swap PrgEnv-cray/7.0.0 PrgEnv-intel/7.0.0
-module load lmod/8.3
 module load cmake/3.16.5-intel
 module load git/2.27.0-intel
 module load expat/2.2.9-intel
@@ -40,10 +41,9 @@ export MPI_CC=$SERIAL_CC
 export MPI_FC=$SERIAL_FC
 export MPI_CXX=$SERIAL_CXX
 
-# WCOSS2 specific
+# LMod is coming to WCOSS2
 # LMod has disabled "default" and requires exact module match.
 # https://lmod.readthedocs.io/en/latest/090_configuring_lmod.html
-export LMOD_EXACT_MATCH="no"
-export LMOD_EXTENDED_DEFAULT="yes"
-
-# Load these basic modules
+#module load lmod/8.3
+#export LMOD_EXACT_MATCH="no"
+#export LMOD_EXTENDED_DEFAULT="yes"
