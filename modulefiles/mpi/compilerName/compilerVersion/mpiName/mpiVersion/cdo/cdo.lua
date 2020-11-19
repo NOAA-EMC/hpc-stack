dcea47fcb968125c3a5ac0660f2b50110cda403b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-11-19 14:35:01 -0500

    add CDO

---
+++ modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/cdo/cdo.lua
@@ -0,0 +1,28 @@
+help([[
+]])
+
+local pkgName    = myModuleName()
+local pkgVersion = myModuleVersion()
+local pkgNameVer = myModuleFullName()
+
+local hierA        = hierarchyA(pkgNameVer,2)
+local mpiNameVer   = hierA[1]
+local compNameVer  = hierA[2]
+local mpiNameVerD  = mpiNameVer:gsub("/","-")
+local compNameVerD = compNameVer:gsub("/","-")
+
+conflict(pkgName)
+
+local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
+
+local base = pathJoin(opt,compNameVerD,mpiNameVerD,pkgName,pkgVersion)
+
+prepend_path("PATH", pathJoin(base,"bin"))
+
+setenv("CDO_ROOT", base)
+setenv("CDO_VERSION", pkgVersion)
+
+whatis("Name: ".. pkgName)
+whatis("Version: " .. pkgVersion)
+whatis("Category: Utilities")
+whatis("Description: Climate Data Operators (CDO) Utility")
