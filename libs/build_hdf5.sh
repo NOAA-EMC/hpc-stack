5eae00deb1d20421a9e620f655fb65b5e4c77310
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-12-09 17:05:54 -0500

    bugfix in build_hdf5.sh with szip.

--- libs/build_hdf5.sh
+++ libs/build_hdf5.sh
@@ -63,7 +63,7 @@ mkdir -p build && cd build
 [[ -z $mpi ]] || extra_conf="--enable-parallel --enable-unsupported"
 
 [[ $enable_shared =~ [yYtT] ]] || shared_flags="--disable-shared --enable-static --enable-static-exec"
-[[ $enable_szip =~ [yYtT] ]] && szip_flags="--with-szip=$SZIP_ROOT"
+[[ $enable_szip =~ [yYtT] ]] && szip_flags="--with-szlib=$SZIP_ROOT"
 [[ $enable_zlib =~ [yYtT] ]] && zlib_flags="--with-zlib=$ZLIB_ROOT"
 
 ../configure --prefix=$prefix \
