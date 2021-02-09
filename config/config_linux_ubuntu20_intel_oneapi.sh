892a8cffcf4759f767c6e71a4a82f381f89a7346
Steve Lawrence <57503604+slawrence87544@users.noreply.github.com>
2021-02-08 21:09:22 -0700

    feat: add support for a unified hpc-stack and jedi-stack (#145)
    * feat: add support for a unified hpc-stack and jedi-stack

--- config/config_linux_ubuntu20_intel_oneapi.sh
+++ config/config_linux_ubuntu20_intel_oneapi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Compiler/MPI combination
-export HPC_COMPILER=${HPC_COMPILER:-"intel/oneapi-beta09"}
-export HPC_MPI=${HPC_MPI:-"impi/oneapi-beta09"}
+export HPC_COMPILER=${HPC_COMPILER:-"intel/oneapi-2021.1.1"}
+export HPC_MPI=${HPC_MPI:-"impi/oneapi-2021.1.1"}
 
 # Build options
 export USE_SUDO=N
