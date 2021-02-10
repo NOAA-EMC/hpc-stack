7d6dc1abe497ce124cbf2dece408271f1e3510ed
Kyle Gerheiser <3209794+kgerheiser@users.noreply.github.com>
2021-02-10 08:52:05 -0500

    Differentiate between lib and lib64 in modules (#154)

--- modulefiles/compiler/compilerName/compilerVersion/jasper/jasper.lua
+++ modulefiles/compiler/compilerName/compilerVersion/jasper/jasper.lua
@@ -15,17 +15,16 @@ local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
 
 local base = pathJoin(opt,compNameVerD,pkgName,pkgVersion)
 
-prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib"))
-prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"lib"))
-prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib64"))
-prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"lib64"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base,"${CMAKE_INSTALL_LIBDIR}"))
+prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"${CMAKE_INSTALL_LIBDIR}"))
+
 prepend_path("CPATH", pathJoin(base,"include"))
 prepend_path("MANPATH", pathJoin(base,"share","man"))
 
 setenv("Jasper_ROOT", base)
 setenv("JASPER_ROOT", base)
 setenv("JASPER_INCLUDES", pathJoin(base,"include"))
-setenv("JASPER_LIBRARIES", pathJoin(base,"lib64"))
+setenv("JASPER_LIBRARIES", pathJoin(base,"${CMAKE_INSTALL_LIBDIR}"))
 setenv("JASPER_VERSION", pkgVersion)
 
 whatis("Name: ".. pkgName)
