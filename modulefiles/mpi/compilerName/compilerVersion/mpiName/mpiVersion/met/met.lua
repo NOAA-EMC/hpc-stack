dc760af09a1b0a61a09d1f134cfa2f766610514e
jprestop <jpresto@ucar.edu>
2021-02-19 00:35:12 +0000

    moved met.lua file under mpi

--- modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/met/met.lua
+++ modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/met/met.lua
@@ -5,8 +5,10 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
-local hierA        = hierarchyA(pkgNameVer,1)
-local compNameVer  = hierA[1]
+local hierA        = hierarchyA(pkgNameVer,2)
+local mpiNameVer   = hierA[1]
+local compNameVer  = hierA[2]
+local mpiNameVerD  = mpiNameVer:gsub("/","-")
 local compNameVerD = compNameVer:gsub("/","-")
 
 conflict(pkgName)
@@ -18,7 +20,7 @@ prereq("python/3.6.3")
 
 local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
 
-local base = pathJoin(opt,compNameVerD,pkgName,pkgVersion)
+local base = pathJoin(opt,compNameVerD,mpiNameVerD,pkgName,pkgVersion)
 
 prepend_path("PATH", pathJoin(base,"bin"))
 prepend_path("MANPATH", pathJoin(base,"share","man"))
