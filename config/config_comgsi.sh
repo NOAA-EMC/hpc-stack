c566b943fc6ac648fd884c93e2ac86a181686e9d
Guoqing Ge <guoqing.ge@noaa.gov>
2021-05-26 09:57:27 -0600

    Add customized config and stack files for community GSI/EnKF (#245)

---
+++ config/config_comgsi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Compiler/MPI combination
+export HPC_COMPILER=${HPC_COMPILER:-"intel/18.0.5"}
+export HPC_MPI=${HPC_MPI:-"impi/2018.4.274"}
+export HPC_PYTHON=${HPC_PYTHON:-"python/3.9.4"}
+
+# Build options
+export USE_SUDO=N
+export PKGDIR=pkg
+export LOGDIR=log
+export OVERWRITE=Y
+export NTHREADS=4
+export   MAKE_CHECK=N
+export MAKE_VERBOSE=N
+export   MAKE_CLEAN=N
+export DOWNLOAD_ONLY=N
+export STACK_EXIT_ON_FAIL=Y
+export WGET="wget -nv"
