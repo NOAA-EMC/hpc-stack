918baf2d69743e6235712783289350b9d7765469
Kyle Gerheiser <kgerheiser@icloud.com>
2021-07-12 10:02:06 -0400

    Update cron testing for updated module structure in the weather model

--- cron-ci/test-ufs.sh
+++ cron-ci/test-ufs.sh
@@ -8,7 +8,8 @@ sed -i "s:module use /.*/stack:module use ${HPC_INSTALL_PATH}/modulefiles/stack:
 sed -i "s:module load \(.*\)/.*:module load \1:" modulefiles/ufs_common*
 
 # give version to ESMF because two versions are built
-sed -i "s:module load esmf:module load esmf/${STACK_esmf_version}:" modulefiles/ufs_common*
+sed -i "s:module load esmf:module load esmf/${STACK_esmf_version}:" modulefiles/ufs_common
+sed -i "s:module load esmf:module load esmf/${STACK_esmf_version}-debug:" modulefiles/ufs_common_debug
 
 cd tests
 ./rt.sh ${UFS_RT_FLAGS}
