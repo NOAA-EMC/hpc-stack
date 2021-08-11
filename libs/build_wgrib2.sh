dbb78fd8aee94368c7197c3a6e64d5609f525337
kgerheiser <kgerheiser@icloud.com>
2021-08-11 14:44:51 -0400

    Fix sed by adding a space

--- libs/build_wgrib2.sh
+++ libs/build_wgrib2.sh
@@ -60,19 +60,19 @@ make clean
 make deep-clean
 
 # Edit makefile with options or defaults
-sed -i'' -e "s:^USE_NETCDF3=.*:USE_NETCDF3=${STACK_wgrib2_netcdf3:-1}:" makefile
-sed -i'' -e "s:^USE_NETCDF4=.*:USE_NETCDF4=${STACK_wgrib2_netcdf4:-0}:" makefile
-sed -i'' -e "s:^USE_REGEX=.*:USE_REGEX=${STACK_wgrib2_negex:-1}:" makefile
-sed -i'' -e "s:^USE_TIGGE=.*:USE_TIGGE=${STACK_wgrib2_tigge:-1}:" makefile
-sed -i'' -e "s:^USE_IPOLATES=.*:USE_IPOLATES=${STACK_wgrib2_ipolates:-3}:" makefile
-sed -i'' -e "s:^USE_SPECTRAL=.*:USE_SPECTRAL=${STACK_wgrib2_spectral:-0}:" makefile
-sed -i'' -e "s:^USE_UDF=.*:USE_AEC=${STACK_wgrib2_udf:-0}:" makefile
-sed -i'' -e "s:^USE_JASPER=.*:USE_JASPER=${STACK_wgrib2_jasper:-1}:" makefile
-sed -i'' -e "s:^USE_OPENMP=.*:USE_OPENMP=${STACK_wgrib2_openmp:-1}:" makefile
-sed -i'' -e "s:^MAKE_FTN_API=.*:MAKE_FTN_API=${STACK_wgrib2_ftn_api:-1}:" makefile
-sed -i'' -e "s:^USE_G2CLIB=.*:USE_G2CLIB=${STACK_wgrib2_g2clib:-0}:" makefile
-sed -i'' -e "s:^USE_PNG=.*:USE_PNG=${STACK_wgrib2_png:-1}:" makefile
-sed -i'' -e "s:^USE_AEC=.*:USE_AEC=${STACK_wgrib2_aec:-1}:" makefile
+sed -i '' -e "s:^USE_NETCDF3=.*:USE_NETCDF3=${STACK_wgrib2_netcdf3:-1}:" makefile
+sed -i '' -e "s:^USE_NETCDF4=.*:USE_NETCDF4=${STACK_wgrib2_netcdf4:-0}:" makefile
+sed -i '' -e "s:^USE_REGEX=.*:USE_REGEX=${STACK_wgrib2_negex:-1}:" makefile
+sed -i '' -e "s:^USE_TIGGE=.*:USE_TIGGE=${STACK_wgrib2_tigge:-1}:" makefile
+sed -i '' -e "s:^USE_IPOLATES=.*:USE_IPOLATES=${STACK_wgrib2_ipolates:-3}:" makefile
+sed -i '' -e "s:^USE_SPECTRAL=.*:USE_SPECTRAL=${STACK_wgrib2_spectral:-0}:" makefile
+sed -i '' -e "s:^USE_UDF=.*:USE_AEC=${STACK_wgrib2_udf:-0}:" makefile
+sed -i '' -e "s:^USE_JASPER=.*:USE_JASPER=${STACK_wgrib2_jasper:-1}:" makefile
+sed -i '' -e "s:^USE_OPENMP=.*:USE_OPENMP=${STACK_wgrib2_openmp:-1}:" makefile
+sed -i '' -e "s:^MAKE_FTN_API=.*:MAKE_FTN_API=${STACK_wgrib2_ftn_api:-1}:" makefile
+sed -i '' -e "s:^USE_G2CLIB=.*:USE_G2CLIB=${STACK_wgrib2_g2clib:-0}:" makefile
+sed -i '' -e "s:^USE_PNG=.*:USE_PNG=${STACK_wgrib2_png:-1}:" makefile
+sed -i '' -e "s:^USE_AEC=.*:USE_AEC=${STACK_wgrib2_aec:-1}:" makefile
 
 make
 
@@ -82,26 +82,26 @@ $SUDO mkdir -p ${prefix}/lib
 $SUDO mkdir -p ${prefix}/bin
 $SUDO mkdir -p ${prefix}/include
 
-$SUDO cp wgrib2/wgrib2 $prefix/bin
+#$SUDO cp wgrib2/wgrib2 $prefix/bin
 
 # Build wgrib2 library with all settings off
 if [[ ${STACK_wgrib2_lib:-n} =~ [yYtT] ]]; then
     make clean
     make deep-clean
 
-    sed -i'' -e "s:^USE_NETCDF3=.*:USE_NETCDF3=0:" makefile
-    sed -i'' -e "s:^USE_NETCDF4=.*:USE_NETCDF4=0:" makefile
-    sed -i'' -e "s:^USE_REGEX=.*:USE_REGEX=1:" makefile
-    sed -i'' -e "s:^USE_TIGGE=.*:USE_TIGGE=1:" makefile
-    sed -i'' -e "s:^USE_IPOLATES=.*:USE_IPOLATES=0:" makefile
-    sed -i'' -e "s:^USE_SPECTRAL=.*:USE_SPECTRAL=0:" makefile
-    sed -i'' -e "s:^USE_UDF=.*:USE_AEC=0:" makefile
-    sed -i'' -e "s:^USE_JASPER=.*:USE_JASPER=0:" makefile
-    sed -i'' -e "s:^USE_OPENMP=.*:USE_OPENMP=0:" makefile
-    sed -i'' -e "s:^MAKE_FTN_API=.*:MAKE_FTN_API=1:" makefile
-    sed -i'' -e "s:^USE_G2CLIB=.*:USE_G2CLIB=0:" makefile
-    sed -i'' -e "s:^USE_PNG=.*:USE_PNG=0:" makefile
-    sed -i'' -e "s:^USE_AEC=.*:USE_AEC=0:" makefile
+    sed -i '' -e "s:^USE_NETCDF3=.*:USE_NETCDF3=0:" makefile
+    sed -i '' -e "s:^USE_NETCDF4=.*:USE_NETCDF4=0:" makefile
+    sed -i '' -e "s:^USE_REGEX=.*:USE_REGEX=1:" makefile
+    sed -i '' -e "s:^USE_TIGGE=.*:USE_TIGGE=1:" makefile
+    sed -i '' -e "s:^USE_IPOLATES=.*:USE_IPOLATES=0:" makefile
+    sed -i '' -e "s:^USE_SPECTRAL=.*:USE_SPECTRAL=0:" makefile
+    sed -i '' -e "s:^USE_UDF=.*:USE_AEC=0:" makefile
+    sed -i '' -e "s:^USE_JASPER=.*:USE_JASPER=0:" makefile
+    sed -i '' -e "s:^USE_OPENMP=.*:USE_OPENMP=0:" makefile
+    sed -i '' -e "s:^MAKE_FTN_API=.*:MAKE_FTN_API=1:" makefile
+    sed -i '' -e "s:^USE_G2CLIB=.*:USE_G2CLIB=0:" makefile
+    sed -i '' -e "s:^USE_PNG=.*:USE_PNG=0:" makefile
+    sed -i '' -e "s:^USE_AEC=.*:USE_AEC=0:" makefile
 
     make lib
 
@@ -110,9 +110,10 @@ if [[ ${STACK_wgrib2_lib:-n} =~ [yYtT] ]]; then
 fi
 
 # Stage CMake package config, fill-in the version, and install
+rm -rf cmake
 mkdir -p cmake
 cp -r ${HPC_STACK_ROOT}/cmake/wgrib2 ./cmake
-sed -i'' -e "s:@WGRIB2_VERSION@:${version}:" ./cmake/wgrib2/wgrib2-config-version.cmake
+sed -i "" -e "s:@WGRIB2_VERSION@:${version}:" ./cmake/wgrib2/wgrib2-config-version.cmake
 $SUDO cp -r cmake ${prefix}/lib
 
 
