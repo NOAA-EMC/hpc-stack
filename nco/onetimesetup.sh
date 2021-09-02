b3da542a66e16f52a6c66c17cb28eaad5e840961
kgerheiser <kgerheiser@icloud.com>
2021-09-02 11:58:37 -0400

    Fix sed

--- nco/onetimesetup.sh
+++ nco/onetimesetup.sh
@@ -2,11 +2,11 @@
 set -eux
 hpcstackdir=$(cd ../ && pwd)
 
-# Remove dash in compiler/mpi name in module
-find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:local compNameVerD = .*::" {} \;
-find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:local mpiNameVerD = .*::" {} \;
+# Remove dash in compiler/mpi name in module so that install tree is <compiler>/<version> instead of <compiler>-<version>
+find $hpcstackdir/modulefiles -type f -name '*.lua" -exec sed -i "s:local compNameVerD = .*::' {} \;
+find $hpcstackdir/modulefiles -type f -name '*.lua" -exec sed -i "s:local mpiNameVerD = .*::' {} \;
 
 # Remove dash from build scripts
-find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i "s:compiler=.*:compiler=$(echo $HPC_COMPILER):" {} \;
-find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i "s:compiler=.*:compiler=$(echo $HPC_MPI):" {} \;
+find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i 's:compiler=$(.*:compiler=$(echo $HPC_COMPILER):' {} \;
+find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i 's:mpi=$(.*:mpi=$(echo $HPC_MPI):' {} \;
 
