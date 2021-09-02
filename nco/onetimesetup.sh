51c9555425b7d52db7378c550267756c3dd49f19
kgerheiser <kgerheiser@icloud.com>
2021-09-02 12:06:41 -0400

    Remove compNameVerD

--- nco/onetimesetup.sh
+++ nco/onetimesetup.sh
@@ -6,6 +6,9 @@ hpcstackdir=$(cd ../ && pwd)
 find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:local compNameVerD = .*::" {} \;
 find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:local mpiNameVerD = .*::" {} \;
 
+find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:compNameVerD:compNameVer:" {} \;
+find $hpcstackdir/modulefiles -type f -name "*.lua" -exec sed -i "s:mpiNameVerD:mpiNameVer:" {} \;
+
 # Remove dash from build scripts
 find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i 's:compiler=$(.*:compiler=$(echo $HPC_COMPILER):' {} \;
 find $hpcstackdir/libs -type f -name "build_*.sh" -exec sed -i 's:mpi=$(.*:mpi=$(echo $HPC_MPI):' {} \;
