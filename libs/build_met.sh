1bc84296deb6e5397879c3b33cc911d3ec050e31
kgerheiser <kgerheiser@icloud.com>
2021-09-08 13:15:51 -0400

    Fix root variables

--- libs/build_met.sh
+++ libs/build_met.sh
@@ -53,18 +53,18 @@ export F77=$FC
 export FFLAGS="${STACK_FFLAGS:-} ${STACK_met_FFLAGS:-}"
 export CFLAGS="${STACK_CFLAGS:-} ${STACK_met_CFLAGS:-}"
 
-export MET_NETCDF=${NetCDF_ROOT}
+export MET_NETCDF=${NETCDF_ROOT}
 export MET_HDF5=${HDF5_ROOT}
 
-bufr_libdir=`find ${bufr_ROOT} -name libbufr_4.a -exec dirname {} \;`
+bufr_libdir=`find ${BUFR_ROOT:-${bufr_ROOT}} -name libbufr_4.a -exec dirname {} \;`
 export MET_BUFRLIB=$bufr_libdir
-g2c_libdir=`find ${g2c_ROOT} -name libg2c.a -exec dirname {} \;`
+g2c_libdir=`find ${G2C_ROOT:-${g2c_ROOT}} -name libg2c.a -exec dirname {} \;`
 export MET_GRIB2CLIB=$g2c_libdir
 export MET_GRIB2CINC=${g2c_ROOT}/include
 export MET_GSL=${GSL_ROOT}
 export BUFRLIB_NAME=-lbufr_4
 export GRIB2CLIB_NAME=-lg2c
-jasper_libdir=`find ${Jasper_ROOT} -name libjasper.a -exec dirname {} \;`
+jasper_libdir=`find ${JASPER_ROOT} -name libjasper.a -exec dirname {} \;`
 export LIB_JASPER=$jasper_libdir
 
 export LIB_LIBPNG=${PNG_ROOT}/lib
@@ -74,7 +74,6 @@ export LIB_Z=${ZLIB_ROOT}/lib
 export MET_PYTHON=${MET_PYTHON:-`which python3`}
 export MET_PYTHON_CONFIG=${MET_PYTHON_CONFIG:-`which python3-config`}
 
-
 if [[ -z ${MET_PYTHON_CC+x} ]]; then
     export MET_PYTHON_CC=`$MET_PYTHON_CONFIG --cflags`
 fi
@@ -83,12 +82,10 @@ if [[ -z ${MET_PYTHON_LD+x} ]]; then
     export MET_PYTHON_LD=`$MET_PYTHON_CONFIG --ldflags`
 fi
 
-
-#LDFLAGS1="-Wl,--disable-new-dtags"
 LDFLAGS2="-Wl,-rpath,${MET_NETCDF}/lib:${MET_HDF5}/lib:${MET_BUFRLIB}"
 LDFLAGS3="-Wl,-rpath,${MET_GRIB2CLIB}:${MET_PYTHON}/lib:${MET_GSL}/lib"
 LDFLAGS4="-L${LIB_JASPER} -L${MET_HDF5}/lib -L${LIB_LIBPNG} -L${LIB_Z}"
-#LDFLAGS5="-L${I_MPI_ROOT}/lib64"
+
 if [[ -z $mpi ]]; then
   export LDFLAGS="${LDFLAGS1:-} ${LDFLAGS2:-} ${LDFLAGS3:-} ${LDFLAGS4:-}"
 else
