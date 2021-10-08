708c6f450dffd607dc4e27e154bdaa803479f331
kgerheiser <kgerheiser@icloud.com>
2021-10-08 11:31:34 -0400

    Add option to disable Python in MET

--- libs/build_met.sh
+++ libs/build_met.sh
@@ -10,12 +10,14 @@ release_date=${2:-${STACK_met_release_date}}
 compiler=$(echo $HPC_COMPILER | sed 's/\//-/g')
 mpi=$(echo $HPC_MPI | sed 's/\//-/g')
 
+[[ $STACK_met_enable_python =~ [yYtT] ]] && enable_python=YES || enable_python=NO
+
 if $MODULES; then
     set +x
     source $MODULESHOME/init/bash
     module load hpc-$HPC_COMPILER
     [[ -z $mpi ]] || module load hpc-$HPC_MPI
-    module load hpc-$HPC_PYTHON
+    [[ $enable_python =~ [yYtT] ]] && module load hpc-$HPC_PYTHON
     module load bufr
     module load zlib
     module load jasper
@@ -74,19 +76,25 @@ export LIB_LIBPNG=${PNG_ROOT}/lib
 export LIB_Z=${ZLIB_ROOT}/lib
 
 
-export MET_PYTHON=${MET_PYTHON:-`which python3`}
-export MET_PYTHON_CONFIG=${MET_PYTHON_CONFIG:-`which python3-config`}
 
-if [[ -z ${MET_PYTHON_CC+x} ]]; then
-    export MET_PYTHON_CC=`$MET_PYTHON_CONFIG --cflags`
-fi
+if [[ $enable_python =~ [yYtT] ]]; then
+    export MET_PYTHON=${MET_PYTHON:-`which python3`}
 
-if [[ -z ${MET_PYTHON_LD+x} ]]; then
-    export MET_PYTHON_LD=`$MET_PYTHON_CONFIG --ldflags`
+    if [[ -z ${MET_PYTHON_CC+x} ]]; then
+        #export MET_PYTHON_CC=`$MET_PYTHON_CONFIG --cflags`
+        echo "Set MET_PYTHON_CC to include 'Python.h' usually through 'python3-config --cflfags'"
+        exit
+    fi
+
+    if [[ -z ${MET_PYTHON_LD+x} ]]; then
+        #export MET_PYTHON_LD=`$MET_PYTHON_CONFIG --ldflags`
+        echo "Set MET_PYTHON_LD to to link to libpython through 'python3-config --ldflags"
+        exit
+    fi
 fi
 
 LDFLAGS2="-L${MET_NETCDF}/lib -L${MET_HDF5}/lib -L${MET_BUFRLIB}"
-LDFLAGS3="-L${MET_GRIB2CLIB} -L${MET_PYTHON}/lib -L${MET_GSL}/lib"
+LDFLAGS3="-L${MET_GRIB2CLIB} -L${MET_GSL}/lib"
 LDFLAGS4="-L${LIB_JASPER} -L${MET_HDF5}/lib -L${LIB_LIBPNG} -L${LIB_Z}"
 
 export LDFLAGS="-fPIE ${LDFLAGS2:-} ${LDFLAGS3:-} ${LDFLAGS4:-}"
@@ -104,7 +112,9 @@ cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}/${pkg_name}
 curr_dir=$(pwd)
 
 
-./configure --prefix=$prefix BUFRLIB_NAME=${BUFRLIB_NAME} GRIB2CLIB_NAME=${GRIB2CLIB_NAME} --enable-grib2 --enable-python
+extra_flags="--enable-grib2 "
+[[ $enable_python =~ [yYtT] ]] && extra_flags+="--enable-python "
+./configure --prefix=$prefix BUFRLIB_NAME=${BUFRLIB_NAME} GRIB2CLIB_NAME=${GRIB2CLIB_NAME} ${extra_flags:-}
 
 make
 [[ $MAKE_CHECK =~ [yYtT] ]] && make check
