d100ba734ff56d8ed8a702467cca455ac3f529fb
kgerheiser <kgerheiser@icloud.com>
2022-02-23 12:41:54 -0500

    Fix syntax in bufr modulefile

--- modulefiles/compiler/compilerName/compilerVersion/bufr/bufr.lua
+++ modulefiles/compiler/compilerName/compilerVersion/bufr/bufr.lua
@@ -16,23 +16,23 @@ local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
 local base = pathJoin(opt,compNameVerD,pkgName,pkgVersion)
 
 local libdir = "lib"
-if (isFile(pathJoin(base,"lib64","libbufr_4.a")))
+if (isFile(pathJoin(base,"lib64","libbufr_4.a"))) then
   libdir = "lib64"
 end
 
 setenv("bufr_ROOT", base)
 setenv("bufr_VERSION", pkgVersion)
-setenv("BUFR_INC4",    pathJoin(base,"include_4"))
-setenv("BUFR_INC8",    pathJoin(base,"include_8"))
-setenv("BUFR_INCd",    pathJoin(base,"include_d"))
+setenv("BUFR_INC4",    pathJoin(base,libdir,"include_4"))
+setenv("BUFR_INC8",    pathJoin(base,libdir,"include_8"))
+setenv("BUFR_INCd",    pathJoin(base,libdir,"include_d"))
 setenv("BUFR_LIB4",    pathJoin(base,libdir,"libbufr_4.a"))
 setenv("BUFR_LIB8",    pathJoin(base,libdir,"libbufr_8.a"))
 setenv("BUFR_LIBd",    pathJoin(base,libdir,"libbufr_d.a"))
 
-if (isDir(pathJoin(base,"include_4_DA")))
-   setenv("BUFR_INC4_DA", pathJoin(base,"include_4_DA"))
-   setenv("BUFR_INC8_DA", pathJoin(base,"include_8_DA"))
-   setenv("BUFR_INCd_DA", pathJoin(base,"include_d_DA"))
+if (isDir(pathJoin(base,"include_4_DA"))) then
+   setenv("BUFR_INC4_DA", pathJoin(base,libdir,"include_4_DA"))
+   setenv("BUFR_INC8_DA", pathJoin(base,libdir,"include_8_DA"))
+   setenv("BUFR_INCd_DA", pathJoin(base,libdir,"include_d_DA"))
    setenv("BUFR_LIB4_DA", pathJoin(base,libdir,"libbufr_4_DA.a"))
    setenv("BUFR_LIB8_DA", pathJoin(base,libdir,"libbufr_8_DA.a"))
    setenv("BUFR_LIBd_DA", pathJoin(base,libdir,"libbufr_d_DA.a"))
