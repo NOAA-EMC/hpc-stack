3fa5067ff8b28b55995727bcfdc5a598325354ba
Natalie Perlin <Natalie@Natalies-MacBook-Air.local>
2022-03-22 11:35:48 -0400

    restored a configuration for macOS with mpich and clang

--- config/config_mac.sh
+++ config/config_mac.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
 
-# Use Lmod for modules definition
-source /usr/local/opt/lmod/init/profile   
-
-module list
-echo "PATH = $PATH"
-
 # Compiler/MPI combination
-export HPC_COMPILER="gnu/11.2.0_3"
+export HPC_COMPILER="clang/12.0.5"
 export HPC_MPI="mpich/3.3.2"
-export HPC_PYTHON="python/3.9.11"
+export HPC_PYTHON="python/3.9.6"
 
 # Build options
 export USE_SUDO=N
 export PKGDIR=pkg
 export LOGDIR=log
 export OVERWRITE=N
-export NTHREADS=4
+export NTHREADS=6
 export   MAKE_CHECK=N
-export MAKE_VERBOSE=Y
+export MAKE_VERBOSE=N
 export   MAKE_CLEAN=N
 export DOWNLOAD_ONLY=N
 export STACK_EXIT_ON_FAIL=Y
-export WGET="wget -nv --no-check-certificate "
+export WGET="wget -nv"
 
-#
 # gfortran-10 needs the following
-export STACK_esmf_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_madis_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_mapl_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-#
-# User specific aliases and functions
-export PATH=/usr/local/bin/:$PATH
-export LIBDIRS=/usr/local/lib
-#
-export CC=/usr/local/bin/gcc
-export FC=/usr/local/bin/gfortran
-export CXX=/usr/local/bin/g++
-#
-export SERIAL_CC=/usr/local/bin/gcc
-export SERIAL_FC=/usr/local/bin/gfortran
-export SERIAL_CXX=/usr/local/bin/g++
+#export STACK_esmf_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
+#export STACK_madis_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
