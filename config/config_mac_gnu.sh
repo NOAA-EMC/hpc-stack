ca065a7d5fd756d16e21a7caa5faf588ce6d6e3a
Natalie Perlin <Natalie@Natalies-MacBook-Air.local>
2022-04-05 12:29:43 -0400

    fixes for ESMF software download and built on Mac OSX

--- config/config_mac_gnu.sh
+++ config/config_mac_gnu.sh
@@ -3,9 +3,6 @@
 # Use Lmod for modules definition
 source /usr/local/opt/lmod/init/profile   
 
-module list
-echo "PATH = $PATH"
-
 # Compiler/MPI combination
 export HPC_COMPILER="gnu/11.2.0_3"
 export HPC_MPI="mpich/3.3.2"
@@ -24,20 +21,15 @@ export DOWNLOAD_ONLY=N
 export STACK_EXIT_ON_FAIL=Y
 export WGET="wget -nv --no-check-certificate "
 
-#
 # gfortran-10 needs the following
-export STACK_esmf_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_madis_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_mapl_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-#
-# User specific aliases and functions
-export PATH=/usr/local/bin/:$PATH
-export LIBDIRS=/usr/local/lib
-#
+export STACK_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz -march=native"
+export STACK_CFLAGS="-march=native"
+export STACK_CXXFLAGS="-march=native"
+ 
 export CC=/usr/local/bin/gcc
 export FC=/usr/local/bin/gfortran
 export CXX=/usr/local/bin/g++
-#
+ 
 export SERIAL_CC=/usr/local/bin/gcc
 export SERIAL_FC=/usr/local/bin/gfortran
 export SERIAL_CXX=/usr/local/bin/g++
--- config/config_mac_m1_gnu.sh
+++ config/config_mac_m1_gnu.sh
@@ -3,9 +3,6 @@
 # Use Lmod for modules definition
 source /opt/homebrew/opt/lmod/init/profile
 
-module list
-echo "PATH = $PATH"
-
 # Compiler/MPI combination
 export HPC_COMPILER="gnu/11.2.0_3"
 export HPC_MPI="openmpi/4.1.2"
@@ -15,7 +12,7 @@ export HPC_PYTHON="python/3.10.2"
 export USE_SUDO=N
 export PKGDIR=pkg
 export LOGDIR=log
-export OVERWRITE=N
+export OVERWRITE=Y
 export NTHREADS=8
 export   MAKE_CHECK=N
 export MAKE_VERBOSE=Y
@@ -26,13 +23,7 @@ export WGET="wget -nv --no-check-certificate "
 
 #
 # gfortran-10 needs the following
-export STACK_esmf_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_madis_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-export STACK_mapl_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
-#
-# User specific aliases and functions
-export PATH=/opt/homebrew/bin/:$PATH
-export LIBDIRS=/opt/homebrew/lib
+export STACK_FFLAGS="-fallow-argument-mismatch -fallow-invalid-boz"
 #
 export CC=/opt/homebrew/bin/gcc
 export FC=/opt/homebrew/bin/gfortran
