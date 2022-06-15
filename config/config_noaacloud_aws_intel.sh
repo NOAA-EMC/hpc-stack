063b7a892f21a9d4bacde21a5607a1362914c613
Parallel Works app-run user <Natalie.Perlin@mgmt-natalieperlin-pclusternoaav2use1-00004.pw-noaa-us-east-1.pw.local>
2022-06-15 11:26:44 +0000

    configuration and stack yaml file for noaacloud_aws_intel

---
+++ config/config_noaacloud_aws_intel.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Compiler/MPI combination
+export HPC_COMPILER="intel/2021.3.0"
+export HPC_MPI="impi/2021.3.0"
+export HPC_PYTHON="miniconda3/4.12.0"
+
+# Build options
+export USE_SUDO=N
+export PKGDIR=pkg
+export LOGDIR=log
+export OVERWRITE=N
+export NTHREADS=4
+export   MAKE_CHECK=N
+export MAKE_VERBOSE=N
+export   MAKE_CLEAN=Y
+export DOWNLOAD_ONLY=N
+export STACK_EXIT_ON_FAIL=Y
+export WGET="wget -nv --no-check-certificate"
+
+module purge
+module load intel/2021.3.0
+module load impi/2021.3.0
+
+# Build FMS with AVX2 flags
+export STACK_fms_CFLAGS="-march=core-avx2"
+export STACK_fms_FFLAGS="-march=core-avx2"
+
+export CC=icc
+export FC=ifort
+export CXX=icpc
+
+export SERIAL_CC=icc
+export SERIAL_FC=ifort
+export SERIAL_CXX=icpc
+
