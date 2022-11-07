fe0faa546801ce4b06aa45cb04d27483e84c8b13
cameronbook <cameron.book@noaa.gov>
2022-11-07 20:31:20 +0000

    Add initial build script for scotch library to support marine/WW3.

---
+++ libs/build_scotch.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -eux
+
+name="scotch"
+version=${1:-${STACK_scotch_version}}
+
+# Hyphenated version used for install prefix
+compiler=$(echo $HPC_COMPILER | sed 's/\//-/g')
+mpi=$(echo $HPC_MPI | sed 's/\//-/g')
+
+if $MODULES; then
+  set +x
+  source $MODULESHOME/init/bash
+  module load hpc-$HPC_COMPILER
+  module load hpc-$HPC_MPI
+  module load cmake/3.20.1
+  module load netcdf/4.9.0
+  module load gnu
+  module list
+  set -x
+  
+  prefix="${PREFIX:-"/opt/modules"}/$compiler/$mpi/$name/$version"
+  if [[ -d $prefix ]]; then
+      if [[ $OVERWRITE =~ [yYtT] ]]; then
+          echo "WARNING: $prefix EXISTS: OVERWRITING!"
+          $SUDO rm -rf $prefix
+      else
+          echo "WARNING: $prefix EXISTS, SKIPPING"
+          exit 0
+      fi
+  fi
+else
+  prefix=${SCOTCH_ROOT:-"/usr/local"}
+fi
+
+
+cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}
+
+software=$name-$version
+URL="https://gitlab.inria.fr/scotch/scotch/-/archive/v${version}/scotch-v${version}.tar.gz"
+[[ -f $software.tar.gz ]] || ( $WGET $URL )
+[[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
+
+cd scotch
+module purge
+module load cmake/3.20.1
+module use /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
+module load hpc/1.2.0
+module load hpc-intel/2022.1.2
+module load hpc-impi/2022.1.2
+module load netcdf/4.9.0
+module load gnu
+
+mkdir build
+cd build
+
+cmake VERBOSE=1 -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX=${SCOTCH_PREFIX}/install -DCMAKE_BUILD_TYPE=Release .. 2>&1 | tee cmake.out
+
+make VERBOSE=1 2>&1 | tee make.out
+make install 2>&1 | tee install.log
+make scotch 2>&1 | tee make_scotch.log
+make ptscotch 2>&1 | tee make_ptscotch.log
