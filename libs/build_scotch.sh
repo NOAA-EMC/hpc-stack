66e32a42822181ae8a74c42ed6636f3d07c23d8e
cameronbook <cameron.book@noaa.gov>
2022-11-08 00:07:29 +0000

    Update scotch build script.

--- libs/build_scotch.sh
+++ libs/build_scotch.sh
@@ -9,6 +9,13 @@ version=${1:-${STACK_scotch_version}}
 compiler=$(echo $HPC_COMPILER | sed 's/\//-/g')
 mpi=$(echo $HPC_MPI | sed 's/\//-/g')
 
+cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}
+
+software=$name-"v"$version
+URL="https://gitlab.inria.fr/scotch/scotch/-/archive/v${version}/scotch-v${version}.tar.gz"
+[[ -f $software.tar.gz ]] || ( $WGET $URL )
+[[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
+
 if $MODULES; then
   set +x
   source $MODULESHOME/init/bash
@@ -34,30 +41,22 @@ else
   prefix=${SCOTCH_ROOT:-"/usr/local"}
 fi
 
+tar -xvf $software.tar.gz; cd $software
+mkdir build; cd build
 
-cd ${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}
+#SCOTCH_PREFIX=${HPC_STACK_ROOT}/${PKGDIR:-"pkg"}/$software
 
-software=$name-$version
-URL="https://gitlab.inria.fr/scotch/scotch/-/archive/v${version}/scotch-v${version}.tar.gz"
-[[ -f $software.tar.gz ]] || ( $WGET $URL )
-[[ ${DOWNLOAD_ONLY} =~ [yYtT] ]] && exit 0
+#cmake VERBOSE=1 -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX=${SCOTCH_PREFIX}/install -DCMAKE_BUILD_TYPE=Release .. 2>&1 | tee cmake.out
 
-cd scotch
-module purge
-module load cmake/3.20.1
-module use /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
-module load hpc/1.2.0
-module load hpc-intel/2022.1.2
-module load hpc-impi/2022.1.2
-module load netcdf/4.9.0
-module load gnu
-
-mkdir build
-cd build
-
-cmake VERBOSE=1 -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX=${SCOTCH_PREFIX}/install -DCMAKE_BUILD_TYPE=Release .. 2>&1 | tee cmake.out
+cmake VERBOSE=1 -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release .. 2>&1 | tee cmake.out
 
 make VERBOSE=1 2>&1 | tee make.out
 make install 2>&1 | tee install.log
 make scotch 2>&1 | tee make_scotch.log
 make ptscotch 2>&1 | tee make_ptscotch.log
+
+
+# generate modulefile from template
+[[ -z $mpi ]] && modpath=compiler || modpath=mpi
+$MODULES && update_modules $modpath $name $version
+echo $name $version $URL >> ${HPC_STACK_ROOT}/hpc-stack-contents.log
