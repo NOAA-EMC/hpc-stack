b4a0b27be13b80dd2c03a379572474035f388d06
Natalie Perlin <Natalie@Natalies-MacBook-Air.local>
2023-08-04 16:57:51 -0400

    update libpng.lua module to allow for lib64 library directory name

--- modulefiles/compiler/compilerName/compilerVersion/libpng/libpng.lua
+++ modulefiles/compiler/compilerName/compilerVersion/libpng/libpng.lua
@@ -15,15 +15,20 @@ local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
 
 local base = pathJoin(opt,compNameVerD,pkgName,pkgVersion)
 
-prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib"))
-prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"lib"))
+local libdir = "lib"
+if (isFile(pathJoin(base,"lib64","libpng.a"))) then
+  libdir = "lib64"
+end
+
+prepend_path("LD_LIBRARY_PATH", pathJoin(base,libdir))
+prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,libdir))
 prepend_path("CPATH", pathJoin(base,"include"))
 prepend_path("MANPATH", pathJoin(base,"share","man"))
 
 setenv("PNG_ROOT", base)
-setenv("LIBPNG_LIB", pathJoin(base,"${CMAKE_INSTALL_LIBDIR}","libpng.a"))
+setenv("LIBPNG_LIB", pathJoin(base,libdir,"libpng.a"))
 setenv("PNG_INCLUDES", pathJoin(base,"include"))
-setenv("PNG_LIBRARIES", pathJoin(base,"lib"))
+setenv("PNG_LIBRARIES", pathJoin(base,libdir))
 setenv("PNG_VERSION", pkgVersion)
 
 whatis("Name: ".. pkgName)
