0de681b9e202fa696c81b496e5c4062a1a7d69e6
Hang-Lei-NOAA <44901908+Hang-Lei-NOAA@users.noreply.github.com>
2024-11-15 13:10:48 -0500

    Update bufr.lua

--- modulefiles/compiler/compilerName/compilerVersion/bufr/bufr.lua
+++ modulefiles/compiler/compilerName/compilerVersion/bufr/bufr.lua
@@ -23,21 +23,7 @@ end
 setenv("bufr_ROOT", base)
 setenv("bufr_VERSION", pkgVersion)
 setenv("BUFR_INC4",    pathJoin(base,libdir,"include_4"))
-setenv("BUFR_INC8",    pathJoin(base,libdir,"include_8"))
-setenv("BUFR_INCd",    pathJoin(base,libdir,"include_d"))
 setenv("BUFR_LIB4",    pathJoin(base,libdir,"libbufr_4.a"))
-setenv("BUFR_LIB8",    pathJoin(base,libdir,"libbufr_8.a"))
-setenv("BUFR_LIBd",    pathJoin(base,libdir,"libbufr_d.a"))
-
-if (isDir(pathJoin(base,"include_4_DA"))) then
-   setenv("BUFR_INC4_DA", pathJoin(base,libdir,"include_4_DA"))
-   setenv("BUFR_INC8_DA", pathJoin(base,libdir,"include_8_DA"))
-   setenv("BUFR_INCd_DA", pathJoin(base,libdir,"include_d_DA"))
-   setenv("BUFR_LIB4_DA", pathJoin(base,libdir,"libbufr_4_DA.a"))
-   setenv("BUFR_LIB8_DA", pathJoin(base,libdir,"libbufr_8_DA.a"))
-   setenv("BUFR_LIBd_DA", pathJoin(base,libdir,"libbufr_d_DA.a"))
-end
-
 prepend_path("PATH", pathJoin(base,"bin"))
 local pydir = pathJoin(base,libdir,"python${PYTHON_VERSION}/site-packages")
 if (isDir(pydir)) then
