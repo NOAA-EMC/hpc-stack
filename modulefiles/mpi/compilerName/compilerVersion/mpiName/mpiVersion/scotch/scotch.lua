88199565002b198c048e8c6f3f6283860c88552c
Hang-Lei-NOAA <44901908+Hang-Lei-NOAA@users.noreply.github.com>
2025-04-21 12:54:15 -0400

    Update scotch.lua

--- modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/scotch/scotch.lua
+++ modulefiles/mpi/compilerName/compilerVersion/mpiName/mpiVersion/scotch/scotch.lua
@@ -18,6 +18,7 @@ local opt = os.getenv("HPC_OPT") or os.getenv("OPT") or "/opt/modules"
 local base = pathJoin(opt,compNameVerD,mpiNameVerD,pkgName,pkgVersion)
 
 prepend_path("PATH", pathJoin(base,"bin"))
+prepend_path("MANPATH", pathJoin(base,"share/man"))
 prepend_path("LIBRARY_PATH", pathJoin(base,"lib64"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib64"))
 prepend_path("DYLD_LIBRARY_PATH", pathJoin(base,"lib64"))
