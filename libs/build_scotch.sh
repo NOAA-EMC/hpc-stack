817316d53111d263c0575a8a468d6096f5494162
hang lei <hang.lei@clogin04.cactus.wcoss2.ncep.noaa.gov>
2025-04-21 16:45:50 +0000

    add sactch 7.0.7

--- libs/build_scotch.sh
+++ libs/build_scotch.sh
@@ -15,11 +15,11 @@ if $MODULES; then
   source $MODULESHOME/init/bash
   module load hpc-$HPC_COMPILER
   module load hpc-$HPC_MPI
-  module load gcc/10.2.0
+  module load gcc
   module load intel/19.1.3.304
   module load PrgEnv-intel/8.1.0
-  module load craype/2.7.13
-  module load cray-mpich/8.1.9
+  module load craype/2.7.17
+  module load cray-mpich/8.1.19
   module load cray-pals/1.2.2
   module list
   set -x
